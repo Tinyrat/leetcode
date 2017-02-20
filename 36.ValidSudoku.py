ca497a46366f0eee56d98e1f9fd4ff132520356c
Tinyrat <490302867@qq.com>
2017-02-20 13:32:28 +0800

    36-38

---
+++ 36.ValidSudoku.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        heng = [[False for i in range(10)] for j in range(9)]
+        shu = [[False for i in range(10)] for j in range(9)]
+        gong = [[False for i in range(10)] for j in range(9)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    num = int(board[i][j])
+                    if heng[i][num] == True:
+                        return False
+                    else:
+                        heng[i][num] = True
+                    if shu[j][num] == True:
+                        return False
+                    else:
+                        shu[j][num] = True
+                    if i < 3 and j < 3:
+                        gongNum = 0
+                    elif i < 3 and j < 6:
+                        gongNum = 1
+                    elif i < 3 and j < 9:
+                        gongNum = 2
+                    elif i < 6 and j < 3:
+                        gongNum = 3
+                    elif i < 6 and j < 6:
+                        gongNum = 4
+                    elif i < 6 and j < 9:
+                        gongNum = 5
+                    elif i < 9 and j < 3:
+                        gongNum = 6
+                    elif i < 9 and j < 6:
+                        gongNum = 7
+                    else:
+                        gongNum = 8
+                    if gong[gongNum][num] == True:
+                        return False
+                    else:
+                        gong[gongNum][num] = True
+        return True
+
+print Solution().isValidSudoku([".87654321","2........","3........","4........","5........","6........","7........","8........","9........"])
---
+++ 38.CountAndSay.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return '1'
+        s = '1'
+        count = 1
+        while count < n:
+            i = 0
+            ret = ''
+            while i < len(s):
+                num = int(s[i])
+                j = i
+                while (j < len(s)) and (s[j] == s[i]):
+                    j += 1
+                ret = ret + str(j - i) + str(num)
+                i = j
+            count += 1
+            s = ret
+        return ret
+
+
+print Solution().countAndSay(5)
